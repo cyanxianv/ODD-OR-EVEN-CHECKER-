cc0e97e632487248405996d06ecd635a6018d3eb
cyanxianv <christianjbvinluan@gmail.com>
2025-03-11 22:27:25 +0800

    Add files via upload

---
+++ LABACT4.py
@@ -0,0 +1,5 @@
+check_number = int(input("Pick a number and if you do I'll tell you if its even or odd!:"))
+if check_number % 2 == 0:
+	print("It is an even number!!")
+else:
+	print("It is an odd number!!")	
